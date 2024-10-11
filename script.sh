0ca93e3a4ae4070e6611c97b37ad5c4148e61714
Dorian <alary.dorian81@gmail.com>
2024-10-11 19:03:10 +0200

    update bugs

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt upgrade
-sudo apt install gcc make gdb ncurses ncurses-devel vim git-all
+sudo apt install gcc make gdb libncurses6 vim git-all
 
 # User env
 cp .bashrc ~/.bashrc
@@ -9,8 +9,10 @@ cp .bashrc ~/.bashrc
 sudo cp vimrc.local /etc/vim/
 
 # git config
+read -p "Enter git name (Ex: Dorian): " name
+read -p "Enter git email (Ex: alary.dorian81@gmail.com): " email
 git config --global user.name $name
 git config --global user.email $email
-git config --gloabl core.editor vim
+git config --global core.editor vim
 git config --global init.defaultBranch main
 git config --global alias.graph "log --oneline --all --graph"
